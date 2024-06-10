f45e09931de3364f14f631483791e061765442c3
Taiki.Yamamoto <taiki.yamamoto@microwavedigital.com>
2024-06-11 00:05:10 +0900

    scss追加

--- gulpfile.js
+++ gulpfile.js
@@ -1,6 +1,20 @@
-function defaultTask(cb) {
-  // place code for your default task here
+// @ts-check
+
+const gulp = require("gulp");
+const sass = require("gulp-sass")(require("sass"));
+
+const { series } = require("gulp");
+
+function clean(cb) {
   cb();
 }
 
-exports.default = defaultTask;
+function build(cb) {
+  return gulp
+    .src("./scss/**/*.scss")
+    .pipe(sass().on("error", sass.logError))
+    .pipe(gulp.dest("./css"));
+}
+
+exports.build = build;
+exports.default = series(clean, build);
