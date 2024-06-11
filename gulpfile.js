cb5a16aa68711bb7505726af91649f2572b4dc1d
Taiki.Yamamoto <taiki.yamamoto@microwavedigital.com>
2024-06-11 23:21:38 +0900

    SCSS出力機能追加

--- gulpfile.js
+++ gulpfile.js
@@ -4,17 +4,28 @@ const gulp = require("gulp");
 const sass = require("gulp-sass")(require("sass"));
 
 const { series } = require("gulp");
+const { watch } = require("gulp");
 
 function clean(cb) {
   cb();
 }
 
-function build(cb) {
+function build() {
   return gulp
-    .src("./scss/**/*.scss")
+    .src("src/assets/scss/**/*.scss")
     .pipe(sass().on("error", sass.logError))
-    .pipe(gulp.dest("./css"));
+    .pipe(gulp.dest("dist/assets/css"));
 }
 
+function scss() {
+  return gulp
+    .src("src/assets/scss/**/*.scss")
+    .pipe(sass().on("error", sass.logError))
+    .pipe(gulp.dest("dist/assets/css"));
+}
+
+exports.default = function () {
+  watch("src/assets/scss/**/*.scss", scss);
+};
+
 exports.build = build;
-exports.default = series(clean, build);
