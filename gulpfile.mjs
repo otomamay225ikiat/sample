22259e270720bc0b88e905ea91b3b54df03c627e
Taiki.Yamamoto <taiki.yamamoto@microwavedigital.com>
2024-06-18 16:00:17 +0900

    ファンクションの用意と記法の変更

--- gulpfile.mjs
+++ gulpfile.mjs
@@ -1,43 +1,84 @@
 // @ts-check
 
-const gulp = require("gulp");
-const sass = require("gulp-sass")(require("sass"));
+import gulp from "gulp";
+import dartSass from "sass";
+import gulpSass from "gulp-sass";
+const sass = gulpSass(dartSass);
+import parallel from "gulp";
 
-const { series } = require("gulp");
-const { watch } = require("gulp");
+import imagemin, { gifsicle, mozjpeg, optipng, svgo } from "gulp-imagemin";
+
+import { watch } from "gulp";
 
 function clean(cb) {
   cb();
 }
 
 function build() {
+  return gulp.parallel([scssBuild, imgBuild, jsBuild]);
+}
+
+function scssDevelop() {
   return gulp
     .src("src/assets/scss/**/*.scss")
     .pipe(sass().on("error", sass.logError))
     .pipe(gulp.dest("dist/assets/css"));
 }
 
-function scss() {
+function imgDevelop() {
+  return gulp
+    .src("src/assets/img/*", { encoding: false })
+    .pipe(gulp.dest("dist/assets/img"));
+}
+
+function jsDevelop(cb) {
+  // body omitted
+  cb();
+}
+
+function scssBuild() {
   return gulp
     .src("src/assets/scss/**/*.scss")
     .pipe(sass().on("error", sass.logError))
     .pipe(gulp.dest("dist/assets/css"));
 }
 
-function img(cb) {
+function imgBuild() {
+  return gulp
+    .src("src/assets/img/*", { encoding: false })
+    .pipe(
+      imagemin([
+        gifsicle({ interlaced: true }),
+        mozjpeg({ quality: 75, progressive: true }),
+        optipng({ optimizationLevel: 5 }),
+        svgo({
+          plugins: [
+            {
+              name: "removeViewBox",
+              active: true,
+            },
+            {
+              name: "cleanupIDs",
+              active: false,
+            },
+          ],
+        }),
+      ]),
+    )
+    .pipe(gulp.dest("dist/assets/img"));
+}
+
+function jsBuild(cb) {
   // body omitted
   cb();
 }
 
-function js(cb) {
-  // body omitted
-  cb();
+function develop() {
+  watch("src/assets/scss/**/*.scss", scssDevelop);
+  watch("src/assets/img/**/*.*", imgDevelop);
+  watch("src/assets/js/**/*.js", jsDevelop);
 }
 
-exports.default = function () {
-  watch("src/assets/scss/**/*.scss", scss);
-  watch(["src/assets/img/**/*.jpg", "src/assets/img/**/*.png"], img);
-  watch("src/assets/js/**/*.js", js);
-};
+export default develop;
 
-exports.build = build;
+export { build };
